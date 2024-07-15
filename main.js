49f7fdd98b59d68ed92f2427c3afee502f70f79a
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-07-15 18:28:54 +0300

    Built the Simple Liker App.

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
-
-
-
+function emptyHeart() {
+  let heartClick = document.querySelector("article footer ul li span");
+  heartClick.addEventListener("click", () => {
+    mimicServerCall()
+      .then((message) => {
+        alert("You notified the server!");
+        alert(message);
+        if (heartClick.textContent === EMPTY_HEART) {
+          heartClick.textContent = FULL_HEART;
+          heartClick.classList.add("activated-heart");
+        } else {
+          heartClick.textContent = EMPTY_HEART;
+          heartClick.classList.remove("activated-heart");
+        }
+      })
+      .catch((error) => {
+        let modal = document.querySelector("#modal");
+        let errorMessage = document.querySelector("div#modal p");
+        errorMessage.textContent = error;
+        modal.classList.remove("hidden");
+        setTimeout(() => {
+          modal.classList.add("hidden");
+        }, 3000);
+      });
+  });
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
@@ -23,3 +46,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+emptyHeart();
